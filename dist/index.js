a00ec7f2852a6011f084a096c445cb9f31d25557
mmdzov <mzov939@gmail.com>
2022-01-25 20:53:10 +0330

    Bug fixes

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const pswd_1 = __importDefault(require("./pswd"));
+var pswd_1 = __importDefault(require("./pswd"));
 exports.default = pswd_1.default;
--- dist/pswd.js
+++ dist/pswd.js
@@ -3,19 +3,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const crypto_js_1 = __importDefault(require("crypto-js"));
-class Pswd {
-    constructor(secret_key) {
+var crypto_js_1 = __importDefault(require("crypto-js"));
+var Pswd = /** @class */ (function () {
+    function Pswd(secret_key) {
         this.secret_key = secret_key;
     }
-    dec(pswd) {
-        const result = crypto_js_1.default.AES.decrypt(pswd, this.secret_key);
-        return result.toString();
-    }
-    enc(message) {
-        const toJSON = JSON.stringify(message);
-        const result = crypto_js_1.default.AES.encrypt(toJSON, this.secret_key).toString();
+    Pswd.prototype.dec = function (pswd) {
+        var result = crypto_js_1.default.AES.decrypt(pswd, this.secret_key);
+        return result.toString(crypto_js_1.default.enc.Utf8).replace(/\\"|\"/gm, "");
+    };
+    Pswd.prototype.enc = function (message) {
+        var toJSON = JSON.stringify(message);
+        var result = crypto_js_1.default.AES.encrypt(toJSON, this.secret_key).toString();
         return result;
-    }
-}
+    };
+    return Pswd;
+}());
 exports.default = Pswd;
