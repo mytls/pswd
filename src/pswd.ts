c1036373d72547101af5e33875aa049ff03484e0
MohammadAli <mzov939@gmail.com>
2022-01-25 16:45:04 +0330

    Bug fixes

--- src/pswd.ts
+++ src/pswd.ts
@@ -1,18 +1,3 @@
-import crypto from "crypto-js";
-
-class Pswd {
-  constructor(public secret_key: string) {}
-
-  dec(pswd: string) {
-    const result = crypto.AES.decrypt(pswd, this.secret_key);
-    return result.toString(crypto.enc.Utf8);
-  }
-
-  enc(message: any): string {
-    const toJSON = JSON.stringify(message);
-    const result = crypto.AES.encrypt(toJSON, this.secret_key).toString();
-    return result;
-  }
-}
+import Pswd from "./index";
 
 export default Pswd;
